803d85114e8e435f22b51b65ba24411f623485c6
Sjoerd Langkemper <sjoerd-github@linuxonly.nl>
2017-01-03 14:52:24 +0100

    Add project files.

---
+++ src/CheckPasswordDictionary/Services/ISmsSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CheckPasswordDictionary.Services
+{
+    public interface ISmsSender
+    {
+        Task SendSmsAsync(string number, string message);
+    }
+}
